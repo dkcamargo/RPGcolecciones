6263ae9fa2c7c2ac96832867ba586a8d5ccf7199
Douglas Camargo <douglas.c@rivka.mx>
2023-04-19 04:33:37 -0300

    Creating Test for Bolsa Class

---
+++ src/TesteBolsa.java
@@ -0,0 +1,30 @@
+public class TesteBolsa {
+    public void ejecutarTests () {
+        testBolsaPrecheck();
+    }
+    
+    public void testBolsaPrecheck() {
+        try {
+            try {
+                Bolsa bolsa = new Bolsa("Mochila",50);
+                
+                System.out.println("Peso libre: " + bolsa.getPesoLibre());
+                
+                Elemento e = new Elemento ("Hueso", 1);
+                bolsa.addElemento(e);
+                e = new Elemento ("Espada", 10);
+                bolsa.addElemento(e);
+                
+                System.out.println("Peso libre: " + bolsa.getPesoLibre());
+                System.out.println(bolsa.getElementosEnLaBolsa());
+                
+                bolsa.delElemento("Hueso");
+                
+                System.out.println("Peso libre: " + bolsa.getPesoLibre());
+                System.out.println(bolsa.getElementosEnLaBolsa());
+            }
+                catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+    }
+}
